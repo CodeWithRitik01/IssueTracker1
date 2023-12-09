36eef2d7d3c14ce4ad91d30ea97b502712f45e46
CodeWithRitik01 <rkritik.9958@gmail.com>
2023-12-09 18:19:50 +0530

    .

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const url = 'mongodb://127.0.0.1:27017/Issue_Tracker';
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(url);
 
 const db = mongoose.connection;
 
