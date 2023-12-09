fb573eb1529e5744f757ef88e947a12f296e7455
CodeWithRitik01 <rkritik.9958@gmail.com>
2023-12-09 19:33:01 +0530

    .

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1/Issue_Tracker';
+const url = 'mongodb://127.0.0.1:27017/Issue_Tracker';
 mongoose.connect(url);
 
 const db = mongoose.connection;
