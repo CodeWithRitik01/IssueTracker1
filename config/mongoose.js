2f4cb990122dbc93c2a8fff1deffbdf0540f00c1
CodeWithRitik01 <rkritik.9958@gmail.com>
2023-12-10 13:19:04 +0530

    yo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const url = 'mongodb://127.0.0.1:27017/Issue_Tracker';
-mongoose.connect(url);
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // 5 seconds timeout for server selection
+    socketTimeoutMS: 45000, // 45 seconds timeout for socket operations
+  });
 
 const db = mongoose.connection;
 
